76a75c88680bf471e37c7d3b55c352fa581cf049
9578686280 <aswinidmerode@gmail.com>
2022-03-08 13:07:15 +0530

    my fike

---
+++ exfor1.java
@@ -0,0 +1,23 @@
+class exfor1
+{
+public static void main(String args[])
+{
+for(int i=0;i<7;i++)
+{
+
+/*for(int j=7;j>i;j--)
+{
+System.out.print(" ");  // space
+}*/
+
+
+
+for(int k=0;k<i;k++)
+{
+System.out.print(" *"); // print 
+}
+
+
+System.out.println();
+}
+}}

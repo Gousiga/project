cc8a40fd83b4cf522b43db4026898a2914366b54
9578686280 <aswinidmerode@gmail.com>
2022-03-08 10:36:34 +0530

    first commit

---
+++ exfor.java
@@ -0,0 +1,33 @@
+class exfor
+{
+public static void main(String args[])
+{
+
+for( int i=5;i>=1;i--)
+{
+
+for( int j=i;j<=5;j++)
+{
+
+System.out.print(j);
+}
+
+System.out.println();
+}
+
+
+}
+
+
+/*
+
+11111
+10001
+10901
+10001
+ 11111   */
+
+
+
+
+  */

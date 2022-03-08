801d5c27f314ab0c5ad0099df63d9670ffbd9778
9578686280 <aswinidmerode@gmail.com>
2022-03-08 10:44:22 +0530

    first commit

---
+++ ascii.java
@@ -0,0 +1,19 @@
+class ascii
+{
+public static void main(String aa[])
+{
+for( int i=0;i<255;i++)
+{
+System.out.print(i +" : "+ (char)i +"  ");
+}
+
+
+
+float f=78.565f;
+
+
+int a=(int)f;
+
+System.out.println(a +"   "  + f);
+}
+}
